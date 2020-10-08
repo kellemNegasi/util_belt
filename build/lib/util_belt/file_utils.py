ae71070397a0ca7180827ee20cc75deb3154c4c3
kellemnegasi <kellemnegasi@gmail.com>
2020-10-09 00:08:07 +0300

    minor fixes

--- build/lib/util_belt/file_utils.py
+++ build/lib/util_belt/file_utils.py
@@ -2,13 +2,13 @@
 
 
 def list_to_txt(list_object,file_dest='./list.txt'):
-    with open(file_dest, 'w') as f:
+    with open('your_file.txt', 'w') as f:
         for item in list_object:
             f.write("%s\n" % item)
-        print(f'written a list of length {len(list_object)} to {file_dest}')
+        print(f'written a list of length{len(list_object)} to at {file_dest}')
 
 def list_from_txt(file_src):
     places = []
     # open file and read the content in a list
-    with open(file_src, 'r') as filehandle:
+    with open('listfile.txt', 'r') as filehandle:
         places = [current_place.rstrip() for current_place in filehandle.readlines()]
