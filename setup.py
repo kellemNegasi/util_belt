43bf52108d9bc0b73ba52d0e1309b401c1c34c6b
kellemnegasi <kellemnegasi@gmail.com>
2020-10-10 14:42:59 +0300

    added and fixed pretty_arry funtion in display_utils module

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r",encoding='utf-8') as fh:
 
 setuptools.setup(
     name="util_belt", # Replace with your own username
-    version="0.0.7",
+    version="0.0.8",
     author="kellem negasi",
     author_email="kellemnegasi@gmail.com",
     description="A package of differnt utilities",
